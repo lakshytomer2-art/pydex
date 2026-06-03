bd086f5faf5cbfe4651e474221a9cad94b446183
lakshytomer2-art <lakshytomer2@gmail.com>
2026-06-04 04:39:45 +0530

    Initial commit

---
+++ py1.py
@@ -0,0 +1,40 @@
+"""#airthmetic operators
+a = 12
+b = 5
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b) #remainder modulus
+print(a**b)#a^b
+
+#relational operators
+a=50
+b=80
+
+print(a==b).          multi line comment
+print(a!=b)
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
+
+#assignment operators
+a=5
+print(a)
+a += 5
+print(a)
+a -= 5
+print(a)
+a *= 5
+print(a)
+a /= 5
+print(a)
+a %= 5
+print(a) #remainder
+a=2
+a **= 5
+print(a) #modulus"""
+
+#logical operators
