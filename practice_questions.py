7dfda0d6034b0464faff6d4cc5b19b42b3f90ccd
lakshytomer2-art <lakshytomer2@gmail.com>
2026-06-11 12:08:16 +0530

    #4 practice questions

---
+++ practice_questions.py
@@ -0,0 +1,17 @@
+# a=int(input("enter the 1st number:"))
+# b=int(input("enter the 2nd number:"))
+# print("sum =",a+b)
+
+
+# a=float(input("side of a square is"))
+# print("area of a square is",a*a)
+
+
+# a=float(input("1st floating number :"))
+# b=float(input("2nd floating number :"))
+# print("the average of floating point numbers is",(a+b)/2)
+
+
+# a=int(input("enter the 1st number:"))
+# b=int(input("enter the 2nd number:"))
+# print(a>=b)
